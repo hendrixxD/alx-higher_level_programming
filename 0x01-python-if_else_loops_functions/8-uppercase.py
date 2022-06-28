555301b93060631af05b9dc755fc23b2c9c9644c
hendrixxD <maticsjnr@gmail.com>
2022-06-28 16:10:35 -0700

    correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,4 @@
-def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
-    print()
+#!/usr/bin/python3
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return abs(number) % 10

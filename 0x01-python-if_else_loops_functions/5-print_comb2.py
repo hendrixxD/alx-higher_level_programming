6e2afd4cb15a0a4735c421010bf8cc3828a2b211
hendrixxD <maticsjnr@gmail.com>
2022-06-28 15:44:38 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
-for i in range(10):
-    for j in range(10):
-        print('{}{}'.format(i,j), end=', ')
+#!/usr/bin/python3
+for i in range(0, 99):
+    print("{0:02d}".format(i), end=", ")
+print("{:d}".format(99))

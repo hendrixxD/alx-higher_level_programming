879b2e01c709d393c38b618eaa0d199052caec36
hendrixxD <maticsjnr@gmail.com>
2022-07-03 08:30:26 -0700

    2-arg.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def Argv(argv):
+    for i in range(1, len(argv)):
+        if i == 0:
+            print("{:d} arguments.".format(i))
+        elif i == 1:
+            print("{:d} argument:".format(i))
+            print("{:d}: {}".format(i, argv[i]))
+        else:
+            j = 1
+            while j <= i:
+                print("{:d}: {:s}".format(j, argv[j]))
+                j += 1
+
+
+if __name__ == "__main__":
+    import sys
+    Argv(sys.argv)
+

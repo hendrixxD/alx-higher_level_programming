aa4e8f8eb424cf2386eab55e0abdb9d34db51b00
hendrixxD <maticsjnr@gmail.com>
2022-07-19 12:52:42 -0700

    2SPLI

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
-    try:
-        for item in range(x):
+    for item in range(x):
+        try:
             print("{:d}".format(my_list[item]), end="")
             num += 1
-        return num
-
-    except (IndexError, TypeError, ValueError):
-        pass
-        print()
+        except (IndexError, TypeError, ValueError):
+            pass
+    print()
+    return num

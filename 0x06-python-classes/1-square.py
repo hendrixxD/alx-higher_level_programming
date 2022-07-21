f5d2accb6a24a22a40fa136b91d9014e05d924fa
hendrixxD <maticsjnr@gmail.com>
2022-07-21 06:56:34 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/binn/python3
+"""defines a Square base on '0-Square.py'"""
+
+
+class Square:
+    """
+    square with artribute: size
+    """
+    def __init__(self, size):
+        """
+        args:
+            size: private size of the square
+        """
+        self.__size = size

b127282e8cc3da09df321521a27438f716a38e68
hendrixxD <maticsjnr@gmail.com>
2022-07-22 09:37:30 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """class module"""
 class Square:
-
-
     """raise:
         Exception: if size is not type int
                    if size < 0
     """
+
+
     def __init__(self, size=0):
 
         """atrr:
             size(int): private instance
         """"
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:

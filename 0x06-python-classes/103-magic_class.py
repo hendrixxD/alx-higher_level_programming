05b24108207e4da7cca35c0233877c1bf4095b7d
hendrixxD <maticsjnr@gmail.com>
2022-07-22 16:56:23 -0700

    byteCode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!usr/bin/python3
+"""magic class module"""
+
+
+class MagicClass:
+    """bytecode interpretation"""
+
+    def __init__(self, radius=0):
+        """Constructor.
+        Args:
+            RADIUS;radius of circle
+        """
+        if not (isinstance(radius, int) and isinstance(radius, float)):
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """return area of a circle"""
+        return math.pi * self.__radius * self.__radius
+    
+    def circumference(self):
+        """returns the circimference of circle"""
+        return 2 * math.pi * self.__radius

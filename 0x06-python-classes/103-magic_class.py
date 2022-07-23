87d6dcccb661d389a75023b0cf7f4701ceba98a4
hendrixxD <maticsjnr@gmail.com>
2022-07-22 17:18:02 -0700

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,6 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """magic class module"""
+import math
 
 
 class MagicClass:
@@ -10,14 +11,14 @@ class MagicClass:
         Args:
             RADIUS;radius of circle
         """
-        if not (isinstance(radius, int) and isinstance(radius, float)):
-            raise TypeError('radius must be a number')
+        if not isinstance(radius, int) and not isinstance(radius, float):
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
         """return area of a circle"""
         return math.pi * self.__radius * self.__radius
-    
+
     def circumference(self):
         """returns the circimference of circle"""
         return 2 * math.pi * self.__radius

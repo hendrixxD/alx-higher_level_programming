14385d057e65212dddf65d5b3ab2a3d095a21d7a
hendrixxD <maticsjnr@gmail.com>
2022-07-27 01:50:50 -0700

    0-add_correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
 """module that performs integers addition"""
 
+
 def add_integer(a, b=98):
     """Returns sum of two integers
     Args:
-        a: int or float
+    a: int or float
         b: int or float
-
     Raises:
         TypeError: if 'a' or 'b' is not int
-
-    Typecast: if 'a' or 'b' is float 
-
+        Typecast: if 'a' or 'b' is float
     """
-
     if not isinstance(a, (int, float)):
         raise TypeError("'a' must be an integer")
-    if not isinstance(b,(int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("'b' must be an integer")
-    
     return int(a) + int(b)

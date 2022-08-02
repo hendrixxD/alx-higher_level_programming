536be1a0f7860aa795c9e88f609bc617650de2c7
hendrixxD <maticsjnr@gmail.com>
2022-08-02 14:38:05 +0100

    6-.p

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class module definition base on 5-base_geometry.pyy"""
+
+
+class BaseGeometry:
+    """defines an unimplemented class"""
+
+    def area(self):
+        """no args passed"""
+        raise Exception("area() not implemented")

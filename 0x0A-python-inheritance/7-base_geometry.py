bd9cd42116a21e3ab1a9e5692cf95d22aafecc6d
hendrixxD <maticsjnr@gmail.com>
2022-08-02 19:47:25 +0100

    iHopeItsTheFinalCoreection

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
 
     def area(self):
         """No Args passed"""
-        raise Exception("area() not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """validates value entries"""

b0649e4e07facc8c033c51f367cb27e9bdc958f9
hendrixxD <maticsjnr@gmail.com>
2022-08-03 17:46:52 +0100

    finally:

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""defines a square that inherits from previous claesses"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""importing Rectangle class"""
+
+
+class Square(Rectangle):
+    """inherits from Rectangle clss form modification: area() and __str__()"""
+
+    def __init__(self, size):
+        """Args: private instance of class"""
+
+        Rectangle.integer_validator(self,"size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """returns area of sqaure"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """returns the description: [Square] <width>/<height>"""
+
+        return f"[Square] {self.__size}/{self.__size}"

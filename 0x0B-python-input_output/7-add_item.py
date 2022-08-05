6757c829e53264a57ab9ba0df124c04db95b5404
hendrixxD <maticsjnr@gmail.com>
2022-08-05 04:33:16 +0100

    load,save and add

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""module model that perform: load, add amd save"""
+import json
+from sys import argv
+
+
+def save_to_json_file(my_obj, filename):
+    """write my_obj to text file using json repr"""
+
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
+
+
+def load_from_json_file(filename):
+    """creates an object file from a json file"""
+
+    with open(filename) as f:
+        ob_file = json.load(f)
+    return ob_file
+
+
+filename = "add_item.json"
+
+
+def add_item():
+    """add item function"""
+
+    try:
+        new_file = load_from_json_file(filename)
+    except (ValueError, FileNotFoundError, TypeError):
+        new_file = []
+
+    new_file += argv[1:]
+    save_to_json_file(new_file, filename)

7228d6ea7bd075325025a28c7b9119586dbbe230
hendrixxD <maticsjnr@gmail.com>
2022-08-30 10:33:14 +0100

    models/base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""""base class" of all classes in this project.
+the goal is to manage 'id' attribute in all future classes
+and avoid duplicating the the same code"""
+
+
+class Base:
+    """base class with private class attribute
+    and class contructor
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+
+        """if 'id' is not None
+            public instance attribute 'id' is assign to it current value
+            i assume id is and type(int)[no need to test for that]
+
+            otherwise:
+                __nb_object is increamented, and the value is asssigned
+                to 'id' attribute
+        """
+
+        if self.id is not None:
+            self.id = id
+        else:
+            __nb_object += 1
+            self.id = __nb_object

44b779024127cf00ece1ac36bbb1eef2fc8b6dc7
hendrixxD <maticsjnr@gmail.com>
2022-09-07 22:16:28 +0100

    CREATE TABLE

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creates first_table in current database
 USE hbtn_0c_0
-CREATE TABLE IF NOT EXISTS first_table(name VARCHAR(256), id INT))
+CREATE TABLE IF NOT EXISTS first_table(name VARCHAR(256), id INT));

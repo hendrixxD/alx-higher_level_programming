5740db7b4d5cb031453db3d2aa893b26c90d9a20
hendrixxD <maticsjnr@gmail.com>
2022-09-08 08:53:33 +0100

    full table creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -14,4 +14,7 @@
 --   id = 4, name = “George”, score = 8
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VAlUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+INSERT INTO second_table VAlUES(1, "John", 10);
+INSERT INTO second_table VAlUES(2, "Alex", 3);
+INSERT INTO second_table VAlUES(3, "Bob", 14);
+INSERT INTO second_table VAlUES(4, "George", 8);

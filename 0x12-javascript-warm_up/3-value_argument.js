2fa80124920df1f49bf698f74334c08b660fa727
hendrixxD <maticsjnr@gmail.com>
2022-10-05 09:18:52 +0100

    print first argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+require('process');
+
+let index;
+for (index = 0; process.argv[index]; index++);
+    
+if (index > 2) {
+    console.log(process.argv[1]);
+} else {
+    console.log('No argument');
+}
+
+

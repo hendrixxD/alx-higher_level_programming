3d0587fabe89cd7f41c921a22fbebed58d1b9c77
hendrixxD <maticsjnr@gmail.com>
2022-10-05 09:19:18 +0100

    print first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -6,7 +6,7 @@ let index;
 for (index = 0; process.argv[index]; index++);
     
 if (index > 2) {
-    console.log(process.argv[1]);
+    console.log(process.argv[2]);
 } else {
     console.log('No argument');
 }

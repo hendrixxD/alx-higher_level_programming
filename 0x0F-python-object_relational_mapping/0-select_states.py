2bd5f9d1923e85eb2631484a4bc0b3738816f099
hendrixxD <maticsjnr@gmail.com>
2022-10-08 22:12:55 +0100

    list states ordered by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
+"""
+list all states from ordered by id from
+hbtn_0e_0_usa
+"""
 import MySQLdb as sql
 from sys import argv
+
+
 def main():
+    """
+    argument passed when initializing
+    """
 
     USER = argv[1]
     PSSWD = argv[2]
     DB = argv[3]
 
-    d_b =sql.connect(host='localhost', user=USER, passwd=PSSWD, db='DB', port=3306)
+    d_b = sql.connect(
+        host = 'localhost', user = USER,
+        passwd = PSSWD, db = DB, port = 3306)
 
-    #cratong cursor
     cur = d_b.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY(id)")
+    cur.execute("SELECT * FROM states ORDER BY id")
 
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
+
+if __name__ == '__main__':
+    main()

1691a032f62ffb1ef4453ccdc0e76aa2411e89ed
hendrixxD <maticsjnr@gmail.com>
2022-10-09 23:54:43 +0100

    query stat3s from database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,8 +8,9 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == '__main__':
-    engine = create_engine(f"mysql+mysqldb://{argv[1]}:\
-            {argv[2]}@localhost:3306/{argv[3]}", pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
     # Initilize engine
     Base.metadata.create_all(engine)
@@ -17,10 +18,12 @@ if __name__ == '__main__':
     # Create and initialize session
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    #query the database
+
+    # query the database
     all_states = session.query(State).order_by(State.id).all()
 
     # print the queried database
     for state in all_states:
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()

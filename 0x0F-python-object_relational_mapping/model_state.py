b1237668056d0aec44de370878769755ef59bcb4
hendrixxD <maticsjnr@gmail.com>
2022-10-09 13:58:18 +0100

    states model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""first state model"""
+
+#from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+#engine = create_engine('mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa')
+
+class State(Base):
+    """table to be created in hbtn_0e_6_usa database
+    """
+    __table_name__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))

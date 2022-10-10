77e192b571c24c5d6dcb5d3fd2d0a802198c3f7b
hendrixxD <maticsjnr@gmail.com>
2022-10-10 01:36:27 +0100

    query and list the first data in database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+list the first object from hbtn_0e_6_usa db
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # Initilize engine
+    Base.metadata.create_all(engine)
+
+    # Create and initialize session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query the database
+    first_state = session.query(State).order_by(State.id).first()
+
+    # print the queried database
+    if first_state is None:
+        print("Nothing")
+    else:
+        for state in:
+            print("{}: {}".format(state.id, state.name))
+
+    session.close()

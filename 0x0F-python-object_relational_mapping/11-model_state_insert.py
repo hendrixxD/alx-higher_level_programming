77b31b5f0bb4a5f77be67deb7b48f72db88bf8a9
hendrixxD <maticsjnr@gmail.com>
2022-10-10 17:44:52 +0100

    add new objects

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -22,13 +22,18 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # add an obkect to State
+    """
+    Adding a new record
+    """
+    # add an object to State
     state1 = State(name="Lousiana")
-
     # add new state
     session.add(state1)
-
     # commit new changes
     session.commit()
 
     print(state1.id)
+    """
+    new_state_id = session.query(State).order_by(State.id.desc()).first()
+    print(f"{new_state_id.id}")
+    """

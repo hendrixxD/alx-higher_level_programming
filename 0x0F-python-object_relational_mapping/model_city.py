508d19a77100a24b20d463cb114ab64e87a633b2
hendrixxD <maticsjnr@gmail.com>
2022-10-12 02:34:05 +0100

    class for City

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+a Python file similar to model_state.py named model_city.py
+that contains the class definition of a City.
+"""
+
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, String, Integer, ForeignKey
+
+Base = declarative_base()
+"""
+city class inherits from Base clas
+"""
+
+
+class City(Base):
+
+    __tablename__ = 'cities'
+    """
+    Args:
+        id:auto-generated, unique integer
+           can’t be null and is a primary key
+        name:a string of 128 characters and can’t be null
+        state_id: an integer, an integer, cant be null, a foreing key
+    """
+    id = Column(Integer(), unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer(), ForeignKey('states.id'), nullable=False)

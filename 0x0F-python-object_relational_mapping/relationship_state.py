78889c412fa777b7cc0a20115383d0449b7fa0c7
hendrixxD <maticsjnr@gmail.com>
2022-10-12 12:33:43 +0100

    created a relatiosnhip

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """first state model"""
 
-# from sqlalchemy import create_engine
+from sqlalchemy,orm import relationship
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 """
 engine = create_engine('mysql+mysqldb://root:
 root@localhost:3306/hbtn_0e_6_usa')
@@ -20,3 +21,6 @@ class State(Base):
     id = Column(Integer, primary_key=True, unique=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
+
+    # relationship with cities
+    cities = relationship("City", backref="state", cascade="all, delete")

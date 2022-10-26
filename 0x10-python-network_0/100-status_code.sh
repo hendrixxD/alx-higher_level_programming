493fbc541e9bc43b9f359fda2be37e6619ad2dfd
hendrixxD <maticsjnr@gmail.com>
2022-10-26 08:36:46 +0100

    -0 -o mistake

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -0 /dev/null "%{http_code}" "$1"
+curl -s -o /dev/null -I -w "%{http_code}" "$1"

36eb411f175735b313e4cfb14dd52bc7de04ef9f
hendrixxD <maticsjnr@gmail.com>
2022-10-26 08:54:34 +0100

    json request

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a POST request to get the body of the response of a JSON file
-curl -s -H "Content-Type: application/json" -d $"(cat "$2")" "$1"
+curl -sX POST "$1" -H "Content-Type: application/json" -d @$2

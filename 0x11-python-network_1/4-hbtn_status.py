e5aca213ccd7d4e1b3f552ce0fb852274ce8880c
hendrixxD <maticsjnr@gmail.com>
2022-10-28 22:37:11 +0100

    latest

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,6 +3,7 @@
 a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
+
 if __name__ == "__main__":
     response = requests.get('https://intranet.hbtn.io/status').text
     print("Body response:")

438bd8debc504faf09af33f1c54c645dc8cc777b
hendrixxD <maticsjnr@gmail.com>
2022-10-28 20:05:48 +0100

    a script that takes in a URL and return a body response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a python script that takes in a URL, sends a request to he URL and
+displays the body of the response
+"""
+import urllib.request
+import urllib.parse
+from urllib.error import URLError, HTTPError
+from sys import argv
+
+if __name__ == "__main__":
+    res = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(res) as response:
+            print(response.read().decode(encoding="utf-8"))
+        except URLError as err:
+            print("Error code: {}". format(err.code))

386d45a203dba458ec4ffe0faf278f44f991ef47
hendrixxD <maticsjnr@gmail.com>
2022-11-05 00:00:17 +0100

    task 7 corrected

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,12 @@
 
 const arg = parseInt(process.argv[2], 10);
 
-if(isNaN(arg)){
-	console.log('Missing number of occurences');
+if (isNaN(arg)){
+    console.log('Missing number of occurences');
 } else {
-	for(let i = 0; i < arg; i++){
-		console.log('C is fun');
-	}
+    for(let i = 0; i < arg; i++){
+        console.log('C is fun');
+    }
 }
 
 

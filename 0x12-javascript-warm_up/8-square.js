0f761c9f32808670a09e4425ff183b5e54951d10
hendrixxD <maticsjnr@gmail.com>
2022-11-05 08:27:34 +0100

    X length  and X breath

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ const arg = parseInt(process.argv[2]);
 if (isNaN(arg)) {
     console.log('Missing size');
 } else {
-    for (let i = 1; i <= arg; i++) {
-        console.log('X')
+    for (let  i = 0; i <= arg; i++) {
+        console.log('X'.repeat(arg));
     }	    
 }
 

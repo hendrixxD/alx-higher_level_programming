0d3ddcc803d8d37fb56eda300d9387c397f845b4
hendrixxD <maticsjnr@gmail.com>
2022-11-15 09:42:05 +0100

    coreection

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const process = require('process');
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) {
-	console.error(err);
-	    return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });

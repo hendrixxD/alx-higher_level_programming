ac8469ab2e854736750707117b8bdf13ac8b4c28
hendrixxD <maticsjnr@gmail.com>
2022-12-05 14:30:55 +0100

    semi isuesa

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 // prints a factorial of a number
 
-const num = parseInt(process.argv[2], 10);
+const num = parseInt(process.argv[2]);
 
-function factorialix(num) {
-    if (isNaN(num) || num === 0) {
-        console.log(1);
-    }
-    return (num * factorialix(num -1));
+function factorialix (num) {
+  if (isNaN(num) || num === 0) {
+    return (1);
+  }
+  if (num < 0) {
+    return (-1);
+  }
+  return (num * factorialix(num - 1));
 }
-console.log(factorialix(num));
+console.log(Number(factorialix(num)));

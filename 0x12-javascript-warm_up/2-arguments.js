1d1b9ac9f6f4f4e3fddc0b5e665b2808b96d274d
hendrixxD <maticsjnr@gmail.com>
2022-12-05 10:13:16 +0100

    semi isuues

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // handling arguments with js
 
 if (process.argv.length < 3) {
-    console.log('No argument');
-  } else if (process.argv.length === 3) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

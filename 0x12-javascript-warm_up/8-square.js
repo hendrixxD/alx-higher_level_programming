bfe434450b97afa09cbfd12990ac7f0f36520f5f
hendrixxD <maticsjnr@gmail.com>
2022-12-05 08:39:41 +0100

    X repeat X times

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const arg = parseInt(process.argv[2], 10);
 if (isNaN(arg)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i <= arg; i++) {
+  for (let i = 0; i < arg; i++) {
     console.log('X'.repeat(arg));
   }
 }

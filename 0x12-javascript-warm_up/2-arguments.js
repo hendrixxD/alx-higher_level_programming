cdc3bf7220bf182d894357aea9af8067ee44a1af
hendrixxD <maticsjnr@gmail.com>
2022-12-05 09:30:34 +0100

    arguments fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
-require('process');
-
-const argvLen = process.argv.length - 2;
-
-if (argvLen > 1) {
-  console.log('Arguments found');
-} else if (argvLen === 1) {
+if (process.argv.length === 2) {
+  console.log('No Argument');
+} else if (process.argv.length === 3) {
   console.log('Argument Found');
 } else {
-  console.log('No Argument');
+  console.log('Arguments Found');
 }

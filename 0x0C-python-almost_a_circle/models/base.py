a076bfa1a769053dfcd0656ddd8281a1f8d34542
hendrixxD <maticsjnr@gmail.com>
2022-12-09 07:08:49 +0100

    updated to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,8 +31,11 @@ class Base(object):
         changes a python dictionary to a json string equivalent
         """
 
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or len(list_dictionaries) == []:
             return "[]"
+        if (type(list_dictionaries) != list or
+           not all(type(x) == dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
     @classmethod

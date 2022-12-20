579a4bd4b1fd98c9aa388b76305e583714e3cf55
hendrixxD <maticsjnr@gmail.com>
2022-12-20 22:46:53 +0100

    added ; to sql code

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create database
-CREATE DATABASE hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use db created above
-USE hbtn_0d_usa
+USE hbtn_0d_usa;
 -- create table states
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));

0c88e0f4888f706d53f1117e10bffc7ed88bf315
hendrixxD <maticsjnr@gmail.com>
2022-12-20 23:22:11 +0100

    7-cities,sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,4 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use the created db above
 USE hbtn_0d_usa;
 -- create table
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, PRIMARY KEY(id), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
